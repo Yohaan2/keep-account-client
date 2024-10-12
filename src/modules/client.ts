b05ce34dbc88f029bb0bd3bc2c4542132df83f68
yohaan2 <garciayohan57@gmail.com>
2024-10-11 20:58:53 -0400

    add bearer token

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -16,27 +16,57 @@ export const getClients = async () => {
 }
 
 export const getDebtsById = async (id:string) => {
-  return await axiosInstance.get(`/client/${id}/debts`)
+  const token = Cookies.get('access_token')
+  return await axiosInstance.get(`/client/${id}/debts`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
 
 export const addDebt = async (data: AddDebt) => {
+  const token = Cookies.get('access_token')
   const { id, ...restData } = data
-  return await axiosInstance.put(`/client/${id}/record-debt`, restData)
+  return await axiosInstance.put(`/client/${id}/record-debt`, restData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
 
 export const deleteClient = async (id: string) => {
-  return await axiosInstance.delete(`/client/${id}`)
+  const token = Cookies.get('access_token')
+  return await axiosInstance.delete(`/client/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
 
 export const reduceAccount = async (data : {id: string, amount: number}) => {
+  const token = Cookies.get('access_token')
   const {id, amount} = data
-  return await axiosInstance.put(`/client/reduce-account/${id}`, {amount})
+  return await axiosInstance.put(`/client/reduce-account/${id}`, {amount}, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
 
 export const resetAccount = async (id: string) => {
-  return axiosInstance.put(`/client/reset-account/${id}`)
+  const token = Cookies.get('access_token')
+  return axiosInstance.put(`/client/reset-account/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
 
 export const setDollarPrice = async (amount: number) => {
-  return await axiosInstance.post('/client/set-dollar-price', {amount})
+  const token = Cookies.get('access_token')
+  return await axiosInstance.post('/client/set-dollar-price', {amount}, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
 }
